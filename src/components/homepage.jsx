dc3385b5ca05331adceae76fa797afa50a16e0c5
Rajeev Kumar <rajeevk@leewayhertz.com>
2025-12-15 20:24:10 +0530

    products api integrated

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Hero from "./hero";
 import NewArrivalsGrid from "./newArrival";
 import TopSelling from "./topSelling";
 import BrowseByDressStyle from "./dressStyleComponent";
 import OurHappyCustomers from "./reviews";
+import axios from "axios";
 
 const Homepage = () => {
+  const getProducts = async () => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_APP_BASE_URL}/products`,
+        { withCredentials: true }
+      );
+      console.log("response", response);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
   return (
     <>
       <Hero />
