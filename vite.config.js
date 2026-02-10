f108dbc23dbb75ef95ae3e09287e2fcf1ae95b35
Rajeev Kumar <rajeevk@leewayhertz.com>
2026-02-10 09:28:53 +0530

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,4 +12,5 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  base: "/app/",
 });
